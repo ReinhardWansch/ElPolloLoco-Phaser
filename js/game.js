8cb9a0e5e1cf5d5fab588f9f706382489824f515
Reinhard Wansch <reinhard@wansch.at>
2025-04-25 09:59:38 +0200

    add ground

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-let canvasWidth= 720;
-let canvasHeight= 480;
+let canvasWidth = 720;
+let canvasHeight = 480;
 
 class ElPolloLoco extends Phaser.Scene {
 
@@ -14,12 +14,23 @@ class ElPolloLoco extends Phaser.Scene {
 
 
     create() {
-        const background= this.add.image(0, 0, 'background');
+        const background = this.add.image(0, 0, 'background');
         background.setOrigin(0, 0);
         background.setScale(canvasHeight / background.height);
 
+        // Character hinzufügen
         this.character = this.physics.add.sprite(160, 0, 'character');
+        // Hitbox anpassen
+        this.character.body.setSize(248, 555); // Breite: 100px, Höhe: 200px
+        this.character.body.setOffset(154, 587); // Verschiebt die Hitbox um 50px nach rechts und unten
         this.character.setScale(0.25);
+
+        // Boden hinzufügen
+        console.log(canvasWidth); ///DEBUG
+        const ground = this.add.rectangle(0, canvasHeight - 40, 100000, 10, 0x000000, 0); // Transparenter Boden
+        this.physics.add.existing(ground, true); // Füge Physik hinzu (statisch)
+        // Kollision zwischen Charakter und Boden
+        this.physics.add.collider(this.character, ground);
     }
 
 
@@ -38,7 +49,8 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 1600 },
+            debug: true
         }
     }
 };
