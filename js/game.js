055e917ddff5d9ffec56d2ffe8e9bee1a61c7dfe
Reinhard Wansch <reinhard@wansch.at>
2025-04-25 09:23:32 +0200

    add character

--- js/game.js
+++ js/game.js
@@ -5,16 +5,26 @@ class ElPolloLoco extends Phaser.Scene {
 
     preload() {
         this.load.image('background', '/img/complete_background.png');
+
+        this.load.spritesheet('character', '/img/sprites/spritesheet-pepe.png', {
+            frameWidth: 610, // Breite eines Frames
+            frameHeight: 1200 // Höhe eines Frames
+        });
     }
+
 
     create() {
         const background= this.add.image(0, 0, 'background');
         background.setOrigin(0, 0);
         background.setScale(canvasHeight / background.height);
+
+        this.character = this.physics.add.sprite(160, 0, 'character');
+        this.character.setScale(0.25);
     }
 
+
     update() {
-        this.cameras.main.scrollX += 2; // Bewegt die Kamera in jedem Frame um 2 Pixel nach rechts
+        // this.cameras.main.scrollX += 2; ///DEBUG
     }
 
 }
